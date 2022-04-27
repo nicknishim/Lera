21a8002d6324b280437017447251007a163e1aac
Nick Nishim <nicknishim@gmail.com>
2022-04-27 20:34:56 +1000

    Update A.b.cpp

--- A.b.cpp
+++ A.b.cpp
@@ -1,8 +1,7 @@
 # include <iostream>
 using namespace std;
-int main ()
-{
-int D [9], x;
-cout << "x ="; cin >> x;
-for (int i = 0; i <= 9; i++) {D[i]=x; cout << "D (" << i << ") = " << D[i] << endl; x++;}
-}
+int main () {
+  int D [9], x;
+  cout << "x ="; cin >> x;
+  for (int i = 0; i <= 9; i++) {D[i]=x; cout << "D (" << i << ") = " << D[i] << endl; x++;}
+  return 0;}
