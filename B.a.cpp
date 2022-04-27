71e01cb946edf2c07613cc26a3c4d05499fb9967
Nick Nishim <nicknishim@gmail.com>
2022-04-27 20:35:06 +1000

    Update B.a.cpp

--- B.a.cpp
+++ B.a.cpp
@@ -1,8 +1,7 @@
 # include <iostream>
 using namespace std;
-int main ()
-{
+int main () {
 int D [9], x;
 cout << "x ="; cin >> x;
 for (int i = 0; i <= 9; i++) {D[i]=x+(9-i); cout << "D (" << i << ") = " << D[i] << endl;}
-}
+return 0;}
