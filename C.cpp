1c54fd82f16509be9b47672ce7494cbd1e8734fa
Nick Nishim <nicknishim@gmail.com>
2022-04-27 19:50:34 +1000

    +

--- C.cpp
+++ C.cpp
@@ -7,7 +7,7 @@ cout << "a = "; cin >> a;
 cout << "b = "; cin >> b;
 cout << "c = "; cin >> c;
 // обязательная проверка чего там навводил пользователь
-if ((a == b && a > c) || (b == c && b > a) || (a == c && a > b)) {std::cout << "Error, there is no largest number" << std::endl; return 0;};
+if ((a == b && a > c) || (b == c && b > a) || (a == c && a > b)) {cout << "Error, there is no largest number" << endl; return 0;};
 if ((a > b) && (a > c)) {M = a;}
   else if ((b > c) && (b > a)) {M = b;}
     else M = c;
