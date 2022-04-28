1465f218c8a1fadacd6798fb911ec8da45a08824
Nick Nishim <nicknishim@gmail.com>
2022-04-28 10:29:24 +1000

    Update A.a.cpp

--- A.a.cpp
+++ A.a.cpp
@@ -1,7 +1,8 @@
-# include <iostream>
+#include <iostream>
 using namespace std;
+
 int main () {
   int D [9], x;
-  cout << "x ="; cin >> x;
+  cout << "x = "; cin >> x;
   for (int i = 0; i <= 9; i++) {D[i]=x; cout << "D (" << i << ") = " << x << endl;}
   return 0;}
