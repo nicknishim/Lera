e2c407733db1dc1b37cbf36692aae0bfda50df59
Nick Nishim <nicknishim@gmail.com>
2022-04-28 10:29:53 +1000

    Update C.b.cpp

--- C.b.cpp
+++ C.b.cpp
@@ -1,7 +1,8 @@
-# include <iostream>
+#include <iostream>
 using namespace std;
+
 int main () {
-int D [10], x;
-cout << "x = "; cin >> x;
-for (int i = 0; i <= 10; i++) {if (i<=5) {D[i]=x-5+i; cout << "D (" << i << ") = " << D[i] << endl;} else {D[i]=x+5-i; cout << "D (" << i << ") = " << D[i] << endl;}}
-return 0;}
+    int D [10], x;
+    cout << "x = "; cin >> x;
+    for (int i = 0; i <= 10; i++) {if (i<=5) {D[i]=x-5+i; cout << "D (" << i << ") = " << D[i] << endl;} else {D[i]=x+5-i; cout << "D (" << i << ") = " << D[i] << endl;}}
+    return 0;}
