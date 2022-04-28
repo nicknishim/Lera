16a015ea7c659e50fa0802cb73722f91b07f1280
Nick Nishim <nicknishim@gmail.com>
2022-04-28 10:29:34 +1000

    Update A.cpp

--- A.cpp
+++ A.cpp
@@ -1,14 +1,15 @@
-# include <iostream>
+#include <iostream>
 using namespace std;
+
 int main () {
-int N, d0, d1, d2, s=0;
-// необязательное пояснение, что требуется именно трехзначное число
-cout << "Please type the number at range from 100 to 999 - "; cin >> N;
-// необязательная проверка, какое именно число ввели, подходит ли под условия
-if ((N>999) || (N<100)) {cout << "N out of range\n"; return 0;}
-d0 = N / 100;
-d1 = N /10 % 10;
-d2 = N % 10;
-s = d0 + d1 +d2;
-cout << "summ = " << s << endl;
-return 0;}
+    int N, d0, d1, d2, s=0;
+    // необязательное пояснение, что требуется именно трехзначное число
+    cout << "Please type the number at range from 100 to 999 - "; cin >> N;
+    // обязательная проверка чего там навводил пользователь, подходит ли под условия
+    if ((N>999) || (N<100)) {cout << "N out of range\n"; return 0;}
+    d0 = N / 100;
+    d1 = N /10 % 10;
+    d2 = N % 10;
+    s = d0 + d1 +d2;
+    cout << "summ = " << s << endl;
+    return 0;}
