52573cd82290b6c2b19ee9bf55a5544ae6522023
Nick Nishim <nicknishim@gmail.com>
2022-04-28 10:29:45 +1000

    Update B.cpp

--- B.cpp
+++ B.cpp
@@ -1,16 +1,17 @@
-# include <iostream>
+#include <iostream>
 using namespace std;
+
 int main () {
-int N, d0, d1, d2, s;
-// необязательное пояснение, что требуется именно трехзначное число
-cout << "Please type the number at range from 100 to 1000 or range -1000 to -99 -- "; cin >> N;
-//переводим число в натуральное положительное если оно было отрицательным
-N = abs(N);
-// необязательная проверка, какое именно число ввели, подходит ли под условия
-if ((N>999) || (N<100)) {cout << "N out of range\n"; return 0;}
-d0 = N / 100;
-d1 = N /10 % 10;
-d2 = N % 10;
-s = d0 + d1 +d2;
-cout << "summ = " << s << endl;
-return 0;}
+    int N, d0, d1, d2, s;
+    // необязательное пояснение, что требуется именно трехзначное число
+    cout << "Please type the number at range from 100 to 1000 or range -1000 to -99 -- "; cin >> N;
+    //переводим число в натуральное положительное если оно было отрицательным
+    N = abs(N);
+    // обязательная проверка чего там навводил пользователь, подходит ли под условия
+    if ((N>999) || (N<100)) {cout << "N out of range\n"; return 0;}
+    d0 = N / 100;
+    d1 = N /10 % 10;
+    d2 = N % 10;
+    s = d0 + d1 +d2;
+    cout << "summ = " << s << endl;
+    return 0;}
